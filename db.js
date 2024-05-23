452444f067016bb46cd8b90809cecab28ce8de44
Aidan Kane <kaneaidan12@gmail.com>
2024-05-22 20:10:33 -0400

    Making sure installing emacs didnt ruin my entire system

--- db.js
+++ db.js
@@ -19,5 +19,6 @@ const close = () => {
     mongoose.disconnect();
 };
 
+
 module.exports = { connect, close }; //makes it so these functions can be used elsewhere in the code. Such wherabouts will so on and so forth be unbeknownst to I (until the program's done and I can find each use of them if I so please)
 // by exporting them as an object, I can use object destructuring to import them elsewhere in my application like so -> const { connect, close } = require('./db.js');
